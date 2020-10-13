ca449575fff633dd37d21a2fbb7ea798d36ca0f3
matijatomc <55134628+matijatomc@users.noreply.github.com>
2020-10-13 20:24:30 +0200

    Add files via upload

---
+++ tomcMatijaNasljedivanje/tomcMatijaNasljedivanje/ComboBox.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tomcMatijaNasljedivanje
+{
+    class ComboBox : IControl
+    {
+        private int brojac;
+        public int Brojac
+        {
+            get { return brojac; }
+            set { brojac = value; }
+        }
+
+        public void Metoda()
+        {
+            Console.WriteLine("Metoda Combo");
+        }
+    }
+}
---
+++ tomcMatijaNasljedivanje/tomcMatijaNasljedivanje/IControl.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tomcMatijaNasljedivanje
+{
+    interface IControl
+    {
+        void Metoda();
+
+        int Brojac { get; set; }
+    }
+}
